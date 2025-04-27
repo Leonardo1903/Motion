4eafab1b579caf1d1a5d2554e7b43b54a95a81de
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2025-04-27 13:39:23 +0530

    Refactor layout and button styles for navigation to Basics and Exit pages

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,26 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="h-screen w-full bg-neutral-900 flex items-center justify-center flex-col gap-4">
-      <div className="text-neutral-300 text-2xl">Learning Framer Motion</div>
-      <div>
-        <button>
-          <Link href="/basics">
-            <p className="text-neutral-500 px-12 py-4 rounded-lg bg-black shadow-[0px_1px_2px_0px_rgba(255,255,255,0.1)_inset,0px_-1px_2px_0px_rgba(255,255,255,0.1)_inset]">
-              Go to Basics
-            </p>
-          </Link>
-        </button>
+    <div
+      className="h-screen w-full bg-neutral-900 flex items-center justify-center flex-col gap-4"
+      style={{
+        backgroundImage: `radial-gradient(circle at 0.5px 0.5px, rgba(6, 182, 212, 0.2) 0.5px, transparent 0)`,
+        backgroundSize: `8px 8px`,
+        backgroundRepeat: `repeat`,
+      }}
+    >
+      <div className="text-neutral-300 text-4xl">Learning Framer Motion</div>
+      <div className="grid grid-cols-2 gap-4">
+        <Link href="/basics">
+          <p className="text-neutral-500 px-10 py-2 rounded-lg bg-black shadow-[0px_1px_2px_0px_rgba(255,255,255,0.1)_inset,0px_-1px_2px_0px_rgba(255,255,255,0.1)_inset] text-center">
+            Go to Basics
+          </p>
+        </Link>
+        <Link href="/exit">
+          <p className="text-neutral-500 px-10 py-2 rounded-lg bg-black shadow-[0px_1px_2px_0px_rgba(255,255,255,0.1)_inset,0px_-1px_2px_0px_rgba(255,255,255,0.1)_inset] text-center">
+            Go to Exit Animations
+          </p>
+        </Link>
       </div>
     </div>
   );
