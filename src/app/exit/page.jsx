4ff38f1e1cc3e6076f746cc9788a8a3177e5ece1
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2025-04-27 13:39:42 +0530

    Exit Animations

---
+++ src/app/exit/page.jsx
@@ -0,0 +1,9 @@
+import Card from "@/components/Card";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-full bg-gray-50 flex items-center justify-center">
+      <Card />
+    </div>
+  );
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,160 @@
+"use client";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import {
+  X,
+  MessageSquare,
+  ClockIcon,
+  ViewIcon,
+  Volleyball,
+  Plus,
+} from "lucide-react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+
+export default function Card() {
+  const [open, setOpen] = useState(true);
+
+  return (
+    <>
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            initial={{
+              opacity: 0,
+              scale: 0.98,
+              filter: "blur(10px)",
+            }}
+            animate={{
+              opacity: 1,
+              scale: 1,
+              filter: "blur(0px)",
+            }}
+            exit={{
+              opacity: 0,
+              scale: 0.98,
+              filter: "blur(10px)",
+            }}
+            transition={{
+              duration: 0.3,
+              ease: "easeInOut",
+            }}
+            className={cn(
+              "w-72 min-h-[26rem] h-[28rem] rounded-xl text-black",
+              "shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)]",
+              "p-6 flex flex-col bg-white"
+            )}
+          >
+            <h2 className="font-bold text-[10px]">Aceternity UI Components</h2>
+            <p className="text-[10px] text-neutral-600 mt-2">
+              A collection of UI components for building user interfaces with
+              Aceternity.
+            </p>
+            <div className="flex items-center justify-center">
+              <button
+                onClick={() => setOpen(false)}
+                className="flex items-center gap-1 text-[10px] mt-4 shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] rounded-md px-2 py-1"
+              >
+                <Image
+                  src="/logo.png"
+                  alt="Aceternity Logo"
+                  width={50}
+                  height={50}
+                  className="h-4 w-4"
+                />
+                Aceternity
+                <X className="h-3 w-3 text-neutral-400" />
+              </button>
+            </div>
+            <div className="bg-gray-100 flex-1 mt-4 rounded-lg border border-dashed border-neutral-200 relative">
+              <motion.div
+                className="absolute border border-neutral-200 inset-0 h-full w-full bg-white rounded-lg divide-y divide-neutral-200"
+                initial={{
+                  opacity: 0,
+                  scale: 0.98,
+                  filter: "blur(10px)",
+                }}
+                whileHover={{
+                  opacity: 1,
+                  scale: 1.05,
+                  filter: "blur(0px)",
+                }}
+                transition={{
+                  // duration: 0.3,
+                  // ease: "easeInOut",
+                  type: "spring",
+                  stiffness: 100,
+                  damping: 15,
+                }}
+              >
+                <div className="p-4 flex gap-2">
+                  <div className="h-7 w-7 flex-shrink-0 bg-gradient-to-br shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] bg-white rounded-md flex items-center justify-center">
+                    <MessageSquare className="h-4 w-4 text-neutral-600" />
+                  </div>
+                  <div className="flex flex-col">
+                    <p className="text-[8px] font-bold text-neutral-600">
+                      Aceternity UI Components
+                    </p>
+                    <p className="text-neutral-400 text-[8px] mt-1">
+                      A collection of UI components
+                    </p>
+                  </div>
+                </div>
+                <div className="p-4 flex gap-2">
+                  <div className="h-7 w-7 flex-shrink-0 bg-gradient-to-br shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] bg-white rounded-md flex items-center justify-center">
+                    <ClockIcon className="h-4 w-4 text-neutral-600" />
+                  </div>
+                  <div className="flex flex-col">
+                    <p className="text-[8px] font-bold text-neutral-600">
+                      24 hours turnaround
+                    </p>
+                    <p className="text-neutral-400 text-[8px] mt-1">
+                      Super fast delivery at warp speed
+                    </p>
+                  </div>
+                </div>
+                <div className="p-4 flex gap-2">
+                  <div className="h-7 w-7 flex-shrink-0 bg-gradient-to-br shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] bg-white rounded-md flex items-center justify-center">
+                    <ViewIcon className="h-4 w-4 text-neutral-600" />
+                  </div>
+                  <div className="flex flex-col">
+                    <p className="text-[8px] font-bold text-neutral-600">
+                      360 days all around
+                    </p>
+                    <p className="text-neutral-400 text-[8px] mt-1">
+                      We are here to help you 24/7
+                    </p>
+                  </div>
+                </div>
+                <div className="p-4 flex gap-2">
+                  <div className="h-7 w-7 flex-shrink-0 bg-gradient-to-br shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] bg-white rounded-md flex items-center justify-center">
+                    <Volleyball className="h-4 w-4 text-neutral-600" />
+                  </div>
+                  <div className="flex flex-col">
+                    <p className="text-[8px] font-bold text-neutral-600">
+                      360 days all around
+                    </p>
+                    <p className="text-neutral-400 text-[8px] mt-1">
+                      We are here to help you 24/7
+                    </p>
+                  </div>
+                </div>
+
+                <div className="p-4 flex gap-2 items-center justify-center">
+                  <div className="h-4 w-4 flex-shrink-0 bg-gradient-to-br shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] bg-white rounded-md flex items-center justify-center">
+                    <Plus className="h-3 w-3 text-neutral-600" />
+                  </div>
+                  <div className="flex flex-col">
+                    <p className="text-neutral-400 text-[8px] mt-1">
+                      Create Project
+                    </p>
+                  </div>
+                </div>
+              </motion.div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
+  );
+}
